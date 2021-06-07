5bd1e2943f9ac3d660d9d98c2ff03c67b034121a
vasu9893 <64803883+vasu9893@users.noreply.github.com>
2021-06-07 13:32:06 +0530

    day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      initialRoute: "/home",
+      initialRoute: "/login",
       routes: {
         "/": (context) => LoginPage(),
         "/login": (context) => LoginPage(),
